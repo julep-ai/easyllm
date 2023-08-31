06e13f06d977881ee435d433aeda9afba2c98462
Dmitry Paramonov <asmatic075@gmail.com>
2023-08-31 09:43:56 +0300

    feat: Add continue field to chat message schema

--- easyllm/schema/base.py
+++ easyllm/schema/base.py
@@ -2,20 +2,21 @@ import importlib.metadata
 from typing import Literal, Optional
 
 from packaging.version import parse
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 def dump_object(object):
     if parse(importlib.metadata.version("pydantic")) < parse("2.0.0"):
-        return object.dict()
+        return object.dict(by_alias=True)
     else:
-        return object.model_dump(exclude_none=True)
+        return object.model_dump(exclude_none=True, by_alias=True)
 
 
 class ChatMessage(BaseModel):
     role: Literal["user", "assistant", "function", "system", "situation", "thought", "information", "functions", "function_call"]
     content: Optional[str] = None
     name: Optional[str] = None
+    continue_: Optional[bool] = Field(None, alias="continue")
 
 
 class Usage(BaseModel):
